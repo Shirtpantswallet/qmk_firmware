f390b5c1416897a578e8cc1423f617662fb82c0b
Joshua Sorenson <shirt.pants.wallet@gmail.com>
2019-07-20 14:42:37 -0500

    Add Ergodox EZ Colemak DH MK mod keymap
    The Colemak DH MK mod keymap emphasizes homerow and thumb key usage, as
    well as promoting rolls in keyboard shortcuts, Left-Down-Up-Right
    sequence mappings for arrow keys, mouse movements, scrolling, and
    paging.
    
    Signed-off-by: Joshua Sorenson <shirt.pants.wallet@gmail.com>

---
+++ keyboards/ergodox_ez/keymaps/colemak_dh_mk_homerow/config.h
@@ -0,0 +1,19 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#undef TAPPING_TERM
+#define TAPPING_TERM 180
+
+#define PERMISSIVE_HOLD
+
+#define TAPPING_FORCE_HOLD
+
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_MAX_SPEED 14
+
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 30
+
